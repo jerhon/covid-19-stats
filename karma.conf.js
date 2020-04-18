d8f926d6673a2f9b9fb043a8e08a963602bbd9a5
Jeremy Honl <jerhon@users.noreply.github.com>
2020-04-18 10:34:42 -0500

    Update to build for netlify.

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/covid-tracker'),
+      dir: require('path').join(__dirname, './coverage/covid-19-stats'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
